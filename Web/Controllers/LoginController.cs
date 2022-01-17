6aa17c8affd34a143bf9831290f115ed42b45041
MURAT KAVAK <MK_9900@hotmail.com>
2022-01-17 11:02:15 +0300

    login page added

---
+++ Web/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Common.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    //[AllowAnonymous]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            return View(); 
+        }
+    }
+}
--- Web/Startup.cs
+++ Web/Startup.cs
@@ -2,6 +2,7 @@ using Common.Models.Request;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -62,7 +63,7 @@ namespace Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
