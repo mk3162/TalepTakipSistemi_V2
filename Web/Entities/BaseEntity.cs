a99af6896a0f1022327ce6c608c3ec7dff75388f
mk3162 <mk9900@gmail.com>
2022-01-19 23:35:18 +0300

    logo added

--- Web/Entities/BaseEntity.cs
+++ Web/Entities/BaseEntity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Web.Entities
 {
     public class BaseEntity
     {
-
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
     }
 }
--- Web/Entities/Kullanici.cs
+++ Web/Entities/Kullanici.cs
@@ -8,10 +8,8 @@ using Web.Service.Interface;
 
 namespace Web.Entities
 {
-    public class Kullanici: IEntity 
+    public class Kullanici: BaseEntity, IEntity 
     {
-        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int KullaniciId { get; set; }
         public string Kodu { get; set; }
         public string Sifre { get; set; }
         public string AdiSoyadi { get; set; }
